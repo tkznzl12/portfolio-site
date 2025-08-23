f32bfa18102bfd5b44e9a7c5ae37007f3dbf4e1f
tkznzl12 <74891586+tkznzl12@users.noreply.github.com>
2025-08-23 15:05:33 +0900

    메인 레이아웃작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,21 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <section className={styles.main_section}>
-          안녕하세요. 프론트엔드 개발자 정수진 입니다.
+        <section className={`${styles.main_section} ${styles.main1}`}>
+          <div className={styles.main1_left}>
+            <p className={styles.main_title}>안녕하세요, 정수진 입니다.</p>
+            <pre className={styles.main_pre}>
+              유지보수성과 사용자 친화적인 코드를 지향하며
+              <br />
+              어떤 기능이든 구현할 수 있도록 꾸준히 노력하는 프론트엔드
+              개발자입니다.
+            </pre>
+          </div>
+          <div className={styles.main1_right}>
+            <div className={styles.profile_img}>
+              <Image src="/images/profile.png" alt="profile" fill />
+            </div>
+          </div>
         </section>
         <section className={styles.main_section}>
           <p>&quot;한 걸음부터, 모든 변화가 시작된다.&quot;</p>
@@ -27,7 +40,7 @@ export default function Home() {
             className={styles.footer_icon}
             href="https://github.com/tkznzl12"
           >
-            <Image src="/icon/github.svg" alt="github" fill />
+            <Image src="/icon/github.png" alt="github" fill />
           </Link>
           <Link
             className={styles.footer_icon}
